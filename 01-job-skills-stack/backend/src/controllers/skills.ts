8f0201355fce55ed4e67943131ec0aca2da40390
SOunit <workbookwork7@gmail.com>
2022-05-09 12:37:00 -0700

    add get skill, add unique check for create skill

--- 01-job-skills-stack/backend/src/controllers/skills.ts
+++ 01-job-skills-stack/backend/src/controllers/skills.ts
@@ -1,4 +1,5 @@
 import { NextFunction, Request, Response } from "express";
+import { ObjectId } from "mongodb";
 import db from "../db";
 
 // https://www.mongodb.com/compatibility/using-typescript-with-mongodb-tutorial
@@ -6,21 +7,45 @@ const createSkill = async (req: Request, res: Response, _: NextFunction) => {
   const { skill } = req.body;
 
   try {
+    const existingSkill = await db
+      .getDb()
+      .collection("skills")
+      .findOne({ name: new RegExp(skill.name, "i") });
+
+    if (existingSkill) {
+      throw new Error(`${skill.name} already exists!`);
+    }
+
     const response = await db.getDb().collection("skills").insertOne(skill);
 
     response
       ? res.status(201).json({ message: "created a new skill", response })
       : res.status(500).json({ message: "Failed to create a new skill" });
-  } catch (error) {
-    console.error(error);
-    res.status(400).json({ message: "Failed to create a new skill" });
+  } catch (error: any) {
+    res.status(400).json({ message: error.message });
   }
 };
 
 const getSkills = async (req: Request, res: Response, _: NextFunction) => {
   const skills = await db.getDb().collection("skills").find({}).toArray();
-
   res.status(200).json(skills);
 };
 
-export default { createSkill, getSkills };
+const getSkill = async (req: Request, res: Response, _: NextFunction) => {
+  const skillId = req?.params?.skillId;
+
+  try {
+    const query = { _id: new ObjectId(skillId) };
+    const skill = await db.getDb().collection("skills").findOne(query);
+
+    if (skill) {
+      res.status(200).send(skill);
+    }
+  } catch (error) {
+    res
+      .status(404)
+      .send(`Unable to find matching document with id: ${req.params.id}`);
+  }
+};
+
+export default { createSkill, getSkills, getSkill };
--- 01-job-skills-stack/backend/src/routes/skills.ts
+++ 01-job-skills-stack/backend/src/routes/skills.ts
@@ -5,5 +5,6 @@ const router = express.Router();
 
 router.post("/skills/new", skillsController.createSkill);
 router.get("/skills", skillsController.getSkills);
+router.get("/skills/:skillId", skillsController.getSkill);
 
 export default router;
