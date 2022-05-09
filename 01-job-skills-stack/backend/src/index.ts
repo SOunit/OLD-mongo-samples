728d3c9e0ea920121babcb568280474ecd279cbf
SOunit <workbookwork7@gmail.com>
2022-05-09 10:00:06 -0700

    add db connection setup

--- 01-job-skills-stack/backend/src/index.ts
+++ 01-job-skills-stack/backend/src/index.ts
@@ -1,24 +1,21 @@
 import express from "express";
-const mongoose = require("mongoose");
+import db from "./db";
 
 const app = express();
 
 app.get("/", (req, res, next) => {
+  db.getDb().collection("tests").insertOne({ name: "test" });
   res.send("hello");
 });
 
 // process.env.PORT come from Heroku
 const PORT = process.env.PORT || 5000;
 
-try {
-  mongoose
-    .connect(
-      `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.r27pb.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
-    )
-    .then(() => {
-      app.listen(PORT);
-      console.log(`listening on ${PORT}`);
-    });
-} catch (err) {
-  console.log(err);
-}
+db.initDb((err, db) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(`listening on ${PORT}`);
+    app.listen(PORT);
+  }
+});
