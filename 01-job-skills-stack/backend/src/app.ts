58806923b88596e420bbe4515301eb39d52b83d1
SOunit <workbookwork7@gmail.com>
2022-05-09 10:33:42 -0700

    add create-skill logic

--- 01-job-skills-stack/backend/src/app.ts
+++ 01-job-skills-stack/backend/src/app.ts
@@ -1,11 +1,23 @@
 import express from "express";
 import db from "./db";
+import bodyParser from "body-parser";
 
 const app = express();
 
-app.get("/", (req, res, next) => {
+app.use(bodyParser.json());
+
+app.get("/test", (req, res, next) => {
   db.getDb().collection("tests").insertOne({ name: "test" });
   res.send("hello");
+});
+
+// FIXME: add type to request
+app.post("/skills/new", (req, res, next) => {
+  const { skill } = req.body;
+
+  db.getDb().collection("skills").insertOne(skill);
+
+  res.status(200).json({ message: "skill created", skill: {} });
 });
 
 // process.env.PORT come from Heroku
---
+++ 01-job-skills-stack/backend/src/models/skill.ts
@@ -0,0 +1,6 @@
+import { ObjectId } from "mongodb";
+
+export type Skill = {
+  id?: ObjectId;
+  name: string;
+};
